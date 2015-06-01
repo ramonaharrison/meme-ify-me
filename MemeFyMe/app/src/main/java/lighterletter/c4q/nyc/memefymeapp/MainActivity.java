a6778122eec3ef26a2043fa00caf5725a1022812
Luke Lee <samsung3star@gmail.com>
2015-06-01 11:20:43 -0400

    switch to desktop

--- MemeFyMe/app/src/main/java/lighterletter/c4q/nyc/memefymeapp/MainActivity.java
+++ MemeFyMe/app/src/main/java/lighterletter/c4q/nyc/memefymeapp/MainActivity.java
@@ -2,11 +2,13 @@ package lighterletter.c4q.nyc.memefymeapp;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -22,14 +24,71 @@ import java.util.Date;
 public class MainActivity extends ActionBarActivity {
 
     Button buttonCamera;
-    Button buttonGallery;
-    Button buttonMeme;
+    ImageView mImageView;
 
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+
+    Uri uri;
+
+
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(mCurrentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
+
+    String mCurrentPhotoPath;
 
     static final int REQUEST_TAKE_PHOTO = 1;
 
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
 
-    String mCurrentPhotoPath;
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                //takePictureIntent.putExtra("path", mCurrentPhotoPath);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+                        Uri.fromFile(photoFile));
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+    private void setPic() {
+        // Get the dimensions of the View
+        int targetW = mImageView.getWidth();
+        int targetH = mImageView.getHeight();
+
+        // Get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        // Determine how much to scale down the image
+        int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+        Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        mImageView.setImageBitmap(bitmap);
+    }
 
     private File createImageFile() throws IOException {
         // Create an image file name
@@ -49,38 +108,36 @@ public class MainActivity extends ActionBarActivity {
     }
 
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putString("path", mCurrentPhotoPath);
+        super.onSaveInstanceState(outState);
 
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            // Create the File where the photo should go
-            File photoFile = null;
-            try {
-                photoFile = createImageFile();
-            } catch (IOException ex) {
-                // Error occurred while creating the File
-
-            }
-            // Continue only if the File was successfully created
-            if (photoFile != null) {
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
-                        Uri.fromFile(photoFile));
-                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
-            }
-        }
     }
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mImageView = (ImageView) findViewById(R.id.imageView);
+
+        if (savedInstanceState != null) {
+            mCurrentPhotoPath = savedInstanceState.getString("path");
+
+            galleryAddPic();
+            setPic();
+        }
+
+
+
+
         buttonCamera = (Button) findViewById(R.id.button_camera_luke);
         buttonCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 dispatchTakePictureIntent();
+
             }
         });
     }
